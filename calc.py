a26eab61d41becff3b611005a1b2402959732877
Gayathrigayu121 <gayugs121@gmail.com>
2024-12-14 11:55:59 +0530

    sub func

--- calc.py
+++ calc.py
@@ -1,9 +1,12 @@
 def add(a, b):
     print(f'{a} + {b} = {a+b}')
 
+def sub(a, b):
+    print(f'{a} - {b} = {a-b}')
 
 if __name__ == '__main__':
     i = 12
     j = 3
 
     add(i, j)
+    sub(i, j)
